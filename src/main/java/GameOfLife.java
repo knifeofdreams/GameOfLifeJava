133894ce37bee55dc0074a872ba9c527071105d7
judito <judito@spotify.com>
2019-10-09 09:16:27 +0200

    Cells with less than 2 neighbours die of underpopulation

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -1,8 +1,13 @@
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class GameOfLife {
 
   public static ArrayList<Integer> stepGeneration(ArrayList<Integer> population) {
-    return new ArrayList<Integer>();
+    return population
+        .stream()
+        .filter(neighbours -> neighbours >= 2)
+        .collect(Collectors
+                     .toCollection(ArrayList::new));
   }
 }
--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -7,8 +7,22 @@ public class GameOfLifeTest {
 
   @Test
   void emptyPopulationStepsToEmptyPopulation() {
-    final ArrayList<Integer> emptyPopulation = new ArrayList<Integer>();
+    final ArrayList<Integer> emptyPopulation = new ArrayList<>();
     assertEquals(emptyPopulation, GameOfLife.stepGeneration(emptyPopulation));
+  }
+
+  @Test
+  public void cellsWithLessThanTwoNeighboursDie() {
+    final ArrayList<Integer> initialPopulation = new ArrayList<>();
+    initialPopulation.add(2);
+    initialPopulation.add(3);
+    initialPopulation.add(1);
+
+    final ArrayList<Integer> expectedPopulation = new ArrayList<>();
+    expectedPopulation.add(2);
+    expectedPopulation.add(3);
+
+    assertEquals(expectedPopulation, GameOfLife.stepGeneration(initialPopulation));
   }
 
 }
