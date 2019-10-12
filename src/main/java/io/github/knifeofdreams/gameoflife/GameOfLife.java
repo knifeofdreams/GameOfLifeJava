e1d947b2377d585e82c32c6b52e1fcabd3f61782
Stanislav Baiduzhyi <baiduzhyi.devel@gmail.com>
2019-10-12 17:29:53 +0200

    Adding license.

--- src/main/java/io/github/knifeofdreams/gameoflife/GameOfLife.java
+++ src/main/java/io/github/knifeofdreams/gameoflife/GameOfLife.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright © 2019 Judit Ördög-Andrási <knifeofdreams86@gmail.com>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file for more details.
+ */
 package io.github.knifeofdreams.gameoflife;
 
 import java.util.List;
