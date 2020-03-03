de02a7e18db3d7f2ead0a5f1ad06384d10577251
judito <knifeofdreams86@gmail.com>
2020-03-03 20:38:12 +0100

    refactor

--- src/main/java/io/github/knifeofdreams/gameoflife/Histogram.java
+++ src/main/java/io/github/knifeofdreams/gameoflife/Histogram.java
@@ -7,31 +7,57 @@ import static java.lang.Math.max;
 
 public class Histogram {
 
-    public int largestRectangle(List<Integer> histogram) {
-        Stack<Integer> positions = new Stack();
-        Stack<Integer> heights = new Stack();
-        int maxSize = 0;
+    private Stack<Integer> positions = new Stack();
+    private Stack<Integer> heights = new Stack();
+    private List<Integer> histogram;
+    private int maxSize = 0;
 
+    public Histogram(List<Integer> histogram) {
+        this.histogram = histogram;
+    }
+
+    public int sizeOfLargestRectangle() {
         for (int i = 0; i < histogram.size(); i++) {
-            if (heights.empty() || histogram.get(i) > heights.peek()) {
-                positions.push(i);
-                heights.push(histogram.get(i));
+            if (isHigherThanPeek(i)) {
+                pushToStack(histogram.get(i), i);
             } else {
-                int lastPosition = i;
-                while (!heights.empty() && histogram.get(i) <= heights.peek()) {
-                    lastPosition = positions.pop();
-                    maxSize = max(heights.pop() * (i - lastPosition), maxSize);
-                }
-
-                heights.push(histogram.get(i));
-                positions.push(lastPosition);
+                calculateSizesSinceLastShorterBar(i);
             }
         }
-
-        while (!heights.empty()) {
-            maxSize = max(heights.pop() * (histogram.size() - positions.pop()), maxSize);
-        }
+        checkForLargerSizeInStack();
 
         return maxSize;
+    }
+
+    private void calculateSizesSinceLastShorterBar(int i) {
+        int lastPosition = i;
+        while (isLowerOrEqualToPeek(i)) {
+            lastPosition = positions.pop();
+            maxSize = calculateMaxSize(i, lastPosition);
+        }
+        pushToStack(histogram.get(i), lastPosition);
+    }
+
+    private void checkForLargerSizeInStack() {
+        while (!heights.empty()) {
+            maxSize = calculateMaxSize(histogram.size(), positions.pop());
+        }
+    }
+
+    private int calculateMaxSize(int i, int lastPosition) {
+        return max(heights.pop() * (i - lastPosition), maxSize);
+    }
+
+    private boolean isLowerOrEqualToPeek(int i) {
+        return !heights.empty() && histogram.get(i) <= heights.peek();
+    }
+
+    private void pushToStack(Integer height, int position) {
+        heights.push(height);
+        positions.push(position);
+    }
+
+    private boolean isHigherThanPeek(int i) {
+        return heights.empty() || histogram.get(i) > heights.peek();
     }
 }
--- src/test/java/io/github/knifeofdreams/gameoflife/HistogramTest.java
+++ src/test/java/io/github/knifeofdreams/gameoflife/HistogramTest.java
@@ -9,26 +9,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class HistogramTest {
     @Test
     public void test1() {
-        final List<Integer> heights = List.of(2, 1, 5, 6, 2, 3);
-
-        Histogram kata = new Histogram();
-        assertEquals(10, kata.largestRectangle(heights));
+        Histogram kata = new Histogram(List.of(2, 1, 5, 6, 2, 3));
+        assertEquals(10, kata.sizeOfLargestRectangle());
     }
 
     @Test
     public void test2() {
-        final List<Integer> heights = List.of(1, 0, 2, 1, 2);
-
-        Histogram kata = new Histogram();
-        assertEquals(3, kata.largestRectangle(heights));
+        Histogram kata = new Histogram(List.of(1, 0, 2, 1, 2));
+        assertEquals(3, kata.sizeOfLargestRectangle());
     }
 
     @Test
     public void test3() {
-        final List<Integer> heights = List.of(1, 3, 2, 1, 2);
-
-        Histogram kata = new Histogram();
-        assertEquals(5, kata.largestRectangle(heights));
+        Histogram kata = new Histogram(List.of(1, 3, 2, 1, 2));
+        assertEquals(5, kata.sizeOfLargestRectangle());
     }
 
 }
